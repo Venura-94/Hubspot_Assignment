c824ceeb665f15d3c5025f3f60d557929d635976
Venura Pussella <venurapussella123@gmail.com>
2024-05-19 16:27:53 +0530

    modified email return properties

--- main.py
+++ main.py
@@ -33,11 +33,18 @@ def extract_tickets():
             "hs_ticket_priority"
         ],
     }
-    response = requests.post(endpoint, headers=headers, json=params)
-    data = response.json()
 
+    response = requests.post(endpoint, headers=headers, json=params)
+    if response.status_code == 200:
+        print("request successful")
+    else:
+        raise Exception(f"request failed with status code: {response.status_code}")
+    
+    # parse the response
+    data = response.json()
+    # extract the useful information
     ticket_list = []
-    results = data.get('results', [])
+    results = data['results']
     for obj in results:
         extracted_obj = {
             "ticket_id": obj['properties']['hs_object_id'],
@@ -56,32 +63,43 @@ def extract_emails():
     params = {
         "filterGroups": [
             {
-                "filters": [
-                    {
-                        "propertyName": "hs_createdate",
-                        "operator": "BETWEEN",
-                        "value": "2024-05-18T07:00:17.801Z",
-                        "highValue": "2024-05-18T08:00:17.801Z"
-                    }
-                ]
+            "filters": [
+                {
+                "propertyName": "hs_createdate",
+                "operator": "BETWEEN",
+                "value": "2024-05-19T07:00:17.801Z",
+                "highValue" : "2024-05-20T08:00:17.801Z"
+                }
+            ]
             }
         ],
         "properties": [
-            "hs_object_id",
-            "hs_createdate",
-            #"hs_email_subject"
-        ]
-    }
+                "hs_object_id",
+                "hs_createdate",
+                "hs_email_subject",
+                "hs_email_text",
+                "hs_email_status"
+            ]
+        }
+    
     response = requests.post(endpoint, headers=headers, json=params)
+    if response.status_code == 200:
+        print("request successful")
+    else:
+        raise Exception(f"request failed with status code: {response.status_code}")
+    
+    # parse the response
     data = response.json()
-
+    # extract the useful information
     email_list = []
-    results = data.get('results', [])
+    results = data['results']
     for obj in results:
         extracted_obj = {
             "create_date": obj['properties']['hs_createdate'],
             "email_id": obj['properties']['hs_object_id'],
-            #"subject": obj['properties']['hs_email_subject']
+            "email_subject": obj['properties']['hs_email_subject'],
+            "email_text": obj['properties']['hs_email_text'],
+            "email_status": obj['properties']['hs_email_status'],
         }
         email_list.append(extracted_obj)
 
